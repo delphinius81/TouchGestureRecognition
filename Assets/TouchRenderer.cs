c1e6d07ca5785abb58c943e062c13eab9401e18e
Dustin Chertoff <dchertoff@gmail.com>
2016-12-12 11:41:27 -0800

    Draw lines (using Unity LineRenderer) based on touch controller position when hand trigger is down.

--- Assets/TouchRenderer.cs
+++ Assets/TouchRenderer.cs
@@ -8,8 +8,14 @@ using UnityEngine;
 
 public class TouchRenderer : MonoBehaviour {
 
+	public GameObject m_LeftHand;
+	public GameObject m_RightHand;
+
     public LineRenderer m_RightLineRenderer;
     public LineRenderer m_LeftLineRenderer;
+
+	public int m_UpdateEveryNFrames = 5;
+	private int m_FrameNumber = 0;
 
     // Use this for initialization
     void Start() {
@@ -17,16 +23,41 @@ public class TouchRenderer : MonoBehaviour {
     }
 
     void Update() {
-        // if the right grip button is down, draw a line
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch)) {
-            Vector3 rightTouchPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
-            // This position is in tracker space, so it needs to get converted into world space before we plop it into
-            // the line renderer
+		m_FrameNumber = m_FrameNumber % m_UpdateEveryNFrames;
+
+		if (OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.RTouch) > 0.1f) {
+			m_RightLineRenderer.numPositions = 0;
+		}
+
+		if (m_FrameNumber == 0 && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch) > 0.1f) {
+			Vector3 rightTouchPosition = m_RightHand.transform.position;
+			Vector3[] positions = new Vector3[m_RightLineRenderer.numPositions];
+			m_RightLineRenderer.GetPositions(positions);
+			int numPositions = positions.Length;
+			Vector3[] newPostions = new Vector3[numPositions + 1];
+			positions.CopyTo (newPostions, 0);
+			newPostions [numPositions] = rightTouchPosition;
+			m_RightLineRenderer.numPositions = numPositions + 1;
+			m_RightLineRenderer.SetPositions (newPostions);
         }
 
+		if (OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch) > 0.1f) {
+			m_LeftLineRenderer.numPositions = 0;
+		}
+
         // if the left grip button is down, draw a line
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch)) {
-            Vector3 leftTouchPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+		if (m_FrameNumber == 0 && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch) > 0.1f) {
+			Vector3 leftTouchPosition = m_LeftHand.transform.position;
+			Vector3[] positions = new Vector3[m_LeftLineRenderer.numPositions];
+			m_LeftLineRenderer.GetPositions(positions);
+			int numPositions = positions.Length;
+			Vector3[] newPostions = new Vector3[numPositions + 1];
+			positions.CopyTo (newPostions, 0);
+			newPostions [numPositions] = leftTouchPosition;
+			m_LeftLineRenderer.numPositions = numPositions + 1;
+			m_LeftLineRenderer.SetPositions (newPostions);
         }
+
+		m_FrameNumber++;
     }
 }
